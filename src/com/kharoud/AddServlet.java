e599388bdab7866c912661df9f3d172e1c73794f
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-21 21:45:29 -0500

    Saving data through cookies.

--- src/com/kharoud/AddServlet.java
+++ src/com/kharoud/AddServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,18 +18,10 @@ public class AddServlet extends HttpServlet {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
-		int k = i + j;
-//		PrintWriter out = res.getWriter();
-//		out.println("result: "+k);
-//		
-//	    req.setAttribute("k", k);
-//		
-//		
-//		RequestDispatcher rd = req.getRequestDispatcher("sq");
-//		rd.forward(req, res);
-		
-		HttpSession session = req.getSession();
-		session.setAttribute("k", k);
+	    int k = i+j;
+	    
+	    Cookie cookie = new Cookie("k", k+"");
+	    res.addCookie(cookie);
 		
 		res.sendRedirect("sq");
 	}
--- src/com/kharoud/SqServlet.java
+++ src/com/kharoud/SqServlet.java
@@ -3,6 +3,7 @@ package com.kharoud;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,9 +13,16 @@ public class SqServlet extends HttpServlet{
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 	
-		HttpSession session = req.getSession();
-		int k = (int)session.getAttribute("k");
 		
+		Cookie cookie[] = req.getCookies();
+		
+		int k = 0;
+		for(Cookie ele: cookie) {
+			
+			if( ele.getName().equals("k")) {
+				k = Integer.parseInt(ele.getValue());
+			}
+		}
 		k = k*k;
 		PrintWriter out = res.getWriter();
 		out.print("Square is:"+k);
