91fa3d5afbb891468e32820673537c0745531957
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-26 00:13:02 -0500

    Filters.

---
+++ src/com/kharoud/IdFilter.java
@@ -0,0 +1,60 @@
+package com.kharoud;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class IdFilter
+ */
+@WebFilter("/addAlien")
+public class IdFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public IdFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		PrintWriter out = response.getWriter();
+		HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+
+		int aid = Integer.parseInt( httpServletRequest.getParameter("aid"));
+		
+		if( aid > 1) {
+			
+		chain.doFilter(request, response);
+		}else {
+			out.print( "Invalid Input");
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+
+}
